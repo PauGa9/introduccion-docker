d773f79db3770706b46c437900892d5d59b6c65a
Pau Gasol <gasanpau@gmail.com>
2020-01-21 09:48:30 +0100

    Incrementer with NodeJS and Redis

---
+++ nodejs-redis/index.js
@@ -0,0 +1,25 @@
+const redis = require("redis"),
+    client = redis.createClient({host: 'redis'});
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+
+const express = require('express');
+const app = express();
+
+app.get('/incrementa', function (request, response) {
+    client.incr('valor');
+    client.get('valor', function(error, value) {
+        response.send(`El valor es ${value}`);
+    });
+});
+
+const server = app.listen(3000, () => console.log('Escuchando el puerto 3000 (del contenedor o del host?)'));
+
+process.on('SIGTERM', () => {
+    server.end();
+    client.end();
+    console.log('byebye');
+});
